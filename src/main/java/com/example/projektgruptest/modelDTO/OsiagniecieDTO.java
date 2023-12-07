bece38814575257af3327e99f9029f1fcb9ca2e4
LukaszPB <lukasz.kiersnowski.110752@student.pb.edu.pl>
2023-12-07 16:55:46 +0100

    OsiągnięcieDTO

---
+++ src/main/java/com/example/projektgruptest/modelDTO/OsiagniecieDTO.java
@@ -0,0 +1,20 @@
+package com.example.projektgruptest.modelDTO;
+
+import lombok.*;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OsiagniecieDTO {
+    private long idOsiagniecia;
+    private String nazwa;
+    private int iloscPunktow;
+    private Date data;
+    private boolean czyZatwierdzone;
+    private String podKategoriaNazwa;
+    private long idWniosku;
+}
