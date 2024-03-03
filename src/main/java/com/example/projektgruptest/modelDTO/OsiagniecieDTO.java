e07e93772a4acee1cd0810e0969ef6d93bbb4e59
LukaszPB <lukasz.kiersnowski.110752@student.pb.edu.pl>
2024-03-03 22:55:28 +0100

    Dodanie walidacji do Osiągnięcia.

--- src/main/java/com/example/projektgruptest/modelDTO/OsiagniecieDTO.java
+++ src/main/java/com/example/projektgruptest/modelDTO/OsiagniecieDTO.java
@@ -2,6 +2,7 @@ package com.example.projektgruptest.modelDTO;
 
 import com.example.projektgruptest.validator.ValidIloscPunktow;
 import com.example.projektgruptest.validator.ValidPodKategoriaExist;
+import com.example.projektgruptest.validator.ValidUserCanAddToWnisek;
 import com.example.projektgruptest.validator.ValidWniosekExist;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
@@ -17,19 +18,15 @@ import java.util.Date;
 @ValidIloscPunktow
 public class OsiagniecieDTO {
     private long idOsiagniecia;
-
     @NotEmpty(message = "Name can't be empty")
     private String nazwa;
     private int iloscPunktow;
-
     @NotNull(message = "Date can't be empty")
     private Date data;
-
     private boolean czyZatwierdzone;
-
-    @NotEmpty(message = "Subcategory can't be empty")
     @ValidPodKategoriaExist
     private String podKategoriaNazwa;
     @ValidWniosekExist
+    @ValidUserCanAddToWnisek
     private long idWniosku;
 }
